01905249ef4a487410799e70fd7850d76d97b479
lamhung81 <lamhung81@gmail.com>
2018-02-11 22:04:16 +0100

    Add integrator for attitude control

--- src/modules/auv_att_control/auv_att_control_main.cpp
+++ src/modules/auv_att_control/auv_att_control_main.cpp
@@ -223,6 +223,10 @@ private:
   float   _Fcx_manual;
   float   _z_depth;
 
+  Vector<3> _z_omega;
+  Vector<3> _Omega_hat;
+  Vector<3> _Delta_G_hat;
+
 
 	float   _Gamma_c_x;
 	float   _Gamma_c_y;
@@ -282,6 +286,7 @@ private:
   	int    	task_main();     
 
     float satFunction(float x, float delta_x);
+    Vector<3> sat3Function(Vector<3> x, float delta_x);
 
 
 };
@@ -306,6 +311,9 @@ AUVAttitudeControl::AUVAttitudeControl():
   _Fcz(0.0),
   _Fcx_manual(0.0),
   _z_depth(0.0),
+  _z_omega(0.0f, 0.0f, 0.0f),
+  _Omega_hat(0.0f, 0.0f, 0.0f),
+  _Delta_G_hat(0.0f, 0.0f, 0.0f),
 
   _Gamma_c_x(0.0f),
   _Gamma_c_y(0.0f),
@@ -566,226 +574,6 @@ float AUVAttitudeControl::joystick_deadband(float joystick_value, float joystick
 	return joystick_value;
 }
 
-// int
-// AUVAttitudeControl::start()
-// {
-
-// 	//subcribe to set_attitude_target topic
-// 	int vehicle_rates_setpoint_sub_fd = orb_subscribe(ORB_ID(vehicle_rates_setpoint));
-// 	// limit the update rate to 5 Hz
-// 	px4_pollfd_struct_t fds = {};
-// 	fds.events = POLLIN; 
-// 	fds.fd = vehicle_rates_setpoint_sub_fd;
-
-
-// 	//bool updated;
-// 	//orb_check(_v_rates_sp_sub, &updated);
-
-// 	//if (updated) {
-// 	//	orb_copy(ORB_ID(vehicle_rates_setpoint), _v_rates_sp_sub, &_v_rates_sp);
-// 	//}
-
-
-//         #if 0  //Debug
-//         orb_set_interval(vehicle_rates_setpoint_sub_fd, 200);
-//         #endif
-
-
-// 	const char *dev= PWM_OUTPUT0_DEVICE_PATH;
-
-// 	/* open for ioctl only */
-// 	int fd = px4_open(dev, 0);
-// 	if (fd < 0) {
-// 			PX4_ERR("can't open %s", dev);
-// 			return 1;
-// 	}
-
-// 	int ret;
-// 	int pwm_value[6]  = {1500, 1500, 1500, 1500, 1500, 1500};
-//         double throttle[6] = {-3.0, -0.5, 0.0, 0.5, 2.5, 4.5 }; //debug, for testing approximation function
-
-//         double Force[3]  = {0.0, 0.0, 0.0}; //debug, for testing 
-//         double Moment[3] = {0.0, 0.0, 0.0}; //debug, for testing 
-
-	
-// 	//int roll_pwm_value , pitch_pwm_value, yaw_pwm_value, thrust_pwm_value ;
-// 	//roll_pwm_value = pitch_pwm_value = yaw_pwm_value = thrust_pwm_value = 1500;
-
-       
-
-// 	while (1) {
-	
-// 		/* wait for sensor update of 1 file descriptor for 10 ms (0.01 second) */
-// 		int poll_ret = px4_poll(&fds, 1, 10);
-		
-//                 (void) poll_ret;  
-
-// 		// timed out - periodic check for _task_should_exit 
-//                 //lhnguyen debug: use the if following code block leads to the reaction of motors 
-//                 //only when there are new messages from vehicle_rates_setpoint
-// 		/*
-//                 if (poll_ret == 0) {
-// 			pwm_value = 1500; //Disarm pwm of BlueESC
-//                         px4_ioctl(fd, PWM_SERVO_SET(0), pwm_value);
-//                         px4_ioctl(fd, PWM_SERVO_SET(1), pwm_value);
-//                         px4_ioctl(fd, PWM_SERVO_SET(2), pwm_value);
-//                         px4_ioctl(fd, PWM_SERVO_SET(3), pwm_value);
-//                         continue;
-// 		}
-//                 */
-
-//                 if (fds.revents & POLLIN) {
-//                 /* obtained data for the first file descriptor */
-//  			struct vehicle_rates_setpoint_s raw;
-// 			memset(&raw, 0, sizeof(raw));
-// 			//copy sensors raw data into local buffer
-// 			orb_copy(ORB_ID(vehicle_rates_setpoint), vehicle_rates_setpoint_sub_fd, &raw);
-			
-
-// 			//Apply joystick deadband, joystick_deadband = 0.1
-//    		 	raw.roll  = joystick_deadband(raw.roll,0.1);
-//    			raw.pitch = joystick_deadband(raw.pitch,0.1);
-//    			raw.yaw   = joystick_deadband(raw.yaw,0.1);
-//    			raw.thrust= joystick_deadband(raw.thrust,0.1);
-
-//    			/* debug lhnguyen
-//                         PX4_INFO("Debug AUV:\t% 1.6f\t %1.6f\t %1.6f\t% 1.6f",
-// 								 (double)raw.roll,
-// 								 (double)raw.pitch,
-// 								 (double)raw.yaw,
-// 								 (double)raw.thrust);
-//                         */
-   			
-//    			//Convert joystick signals to pwm values, 
-//    			//Neutral value =1500, according to T200 Bluerobotics motor characteristic
-//    			//Take 1500 +/- 50 for giving small pwm value range
-//    			/*
-//                         roll_pwm_value   = 1500 + (int)((float)50.0*raw.roll);
-//    			pitch_pwm_value  = 1500 + (int)((float)50.0*raw.pitch);
-//    			yaw_pwm_value    = 1500 + (int)((float)50.0*raw.yaw);
-//    			thrust_pwm_value = 1500 + (int)((float)50.0*raw.thrust); 
-//                         */
-
-//                         /* debug lhnguyen pwm output to motors 
-//                         PX4_INFO("Debug AUV:\t% 6d\t %6d\t %6d\t% 6d",
-//                                                                  roll_pwm_value,
-//                                                                  pitch_pwm_value,
-//                                                                  yaw_pwm_value,
-//                                                                  thrust_pwm_value);*/
-
-//                         Force[0]  = (float)15.0*raw.thrust;
-//                         Force[1]  = 0.0; 
-//                         Force[2]  = 0.0;
-//                         Moment[0] =  (float)2.0*raw.roll;   
-//                         Moment[1] =  (float)2.0*raw.pitch;    
-//                         Moment[2] =  (float)2.0*raw.yaw;   
-//                         /* debug lhnguyen pwm output to motors */
-//                         PX4_INFO("Debug AUV: %1.6f  %1.6f  %1.6f %1.6f ",
-//                                                                  Force[0],
-//                                                                  Moment[0],
-//                                                                  Moment[1],
-//                                                                  Moment[2]);
-                        
-//    		 }
-
-//                 /*
-//                 //Test with some values of Force (in N) and Moment (in N.m)
-//                  Force[0]  =  0.0;  Force[1]   = 0.0; Force[2]  = 0.0;
-//                  Moment[0] =  0.0;   Moment[1] = 0.0; Moment[2] = 2.0;
-//                 */
-
-
-//                  //Calculate throttle (in N) of motors with given Force (N) and Moment (N.m)
-//                  ForceMoment2Throttle(Force, Moment, throttle[0], throttle[1], throttle[2], throttle[3], throttle[4], throttle[5]);
-
-                 
-//                  //Taking into account CW (Clock Wise) or CCW (Counter Clock Wise) directions
-//                  //CW: Thruster 2 and 4
-//                  throttle[1] = 1.0*throttle[1];
-//                  throttle[3] = 1.0*throttle[3];
-                  
-//                  //CCW: Thruster 1, 3 and 6
-//                  throttle[0] = -1.0*throttle[0];
-//                  throttle[2] = -1.0*throttle[2];
-//                  throttle[5] = -1.0*throttle[5];
-
-//                  //Change direction  of thruster 5 (throttle[4]) to fit with long watertight body
-//                  throttle[4] = -1.0*throttle[4];
-                
-
-//                  for (unsigned i = 0; i < 6; i++) {  
-                        
-                        
-
-//                         //convert from  N to kgf
-//                         throttle[i] = (double)throttle[i] / 9.80665;
-
-//                         //throttle = {-3.0, -0.5, 0.0, 0.5, 2.5, 4.5 }; //debug, for testing approximation function
-
-//                         //lookup values, with values defined in kgf
-//                         pwm_value[i] = pwm_lookup_table((double)throttle[i]);
-
-//                         PX4_INFO("PWM_VALUE %d   %5d", i+1, pwm_value[i]);
-//                         ret = px4_ioctl(fd, PWM_SERVO_SET(i), pwm_value[i]);       
-
-//                         if (ret != OK) {
-//                                 PX4_ERR("PWM_SERVO_SET(%d)", i);
-//                                 return 1;
-//                         }                 
-//                  }
-                 
-
-
-//                 /*        
-//                 for (unsigned i = 0; i < 4; i++) {                                     
-//                         switch (i) {
-//                                 case 0:
-//                                         pwm_value = roll_pwm_value;
-//                                         break;
-//                                 case 1:
-//                                         pwm_value = pitch_pwm_value;
-//                                         break;
-//                                 case 2:
-//                                         pwm_value = yaw_pwm_value;
-//                                         break;  
-//                                 case 3:
-//                                         pwm_value = thrust_pwm_value;
-//                                         break;  
-//                                 default:
-//                                         pwm_value = 1500;
-//                                         break;
-//                                 }
-                        
-//                                 // PX4_INFO("PWM_VALUE  %5d", pwm_value);
-//                                 //ret = px4_ioctl(fd, PWM_SERVO_SET(i), pwm_value);
-//                                 ret = px4_ioctl(fd, PWM_SERVO_SET(i), pwm_value);       
-
-//                                 if (ret != OK) {
-//                                         PX4_ERR("PWM_SERVO_SET(%d)", i);
-//                                         return 1;
-//                                 }
-                        
-                   
-//                         }
-//                 */        
-
-//                         /* Delay longer than the max Oneshot duration */
-//                         //usleep(2542*10); //micro second
-
-//                 #ifdef __PX4_NUTTX
-//                         /* Trigger all timer's channels in Oneshot mode to fire
-//                          * the oneshots with updated values.
-//                          */
-//                         up_pwm_update();
-//                 #endif
-
-
-// 		}
-       
-// }
-
-
-
 
 
 void
@@ -947,14 +735,18 @@ AUVAttitudeControl::control_depth(float dt)
 	//Update reference depth value
 	_zr += _vzr*dt;  
 
+  //
+  float depth_top = 0.1;
+  float depth_bottom = 1.0;
+
 	//limit min and max depth 
-	if (_zr <= (float)0.2){
-		_zr  = (float) 0.2;
+	if (_zr <= depth_top){
+		_zr  = depth_top;
 		_vzr = (float) 0.0;
 	} 
 
-	if (_zr >= (float)1.2) {
-		_zr = (float) 1.2;
+	if (_zr >= depth_bottom) {
+		_zr = depth_bottom;
 		_vzr = (float) 0.0;
 	}
 
@@ -999,8 +791,9 @@ AUVAttitudeControl::control_depth(float dt)
   float roll  = Euler_angle_in_rad(0);
   float pitch = Euler_angle_in_rad(1);
 
-  double d_PP1 = 0.01; // Distance from pressure sensor to projection point P1, in meter
-  double L_P1B = 0.3;  // Distance from projection P1 to center of boyancy, in meter
+  //Assume that pressure sensor P is exactly on vertical symmestrical plane
+  double d_PP1 = 0.01; // Distance from pressure sensor to projection point P1, along z axe, in meter
+  double L_P1B = 0.3;  // Distance from projection P1 to center of boyancy, along x axe, in meter
 
   //Depth control is according to center of boyancy.
   //Need to convert the depth and depth_velocity measured at P (end of the tube) to the value of B with taking into account AUV rotation kinematics
@@ -1026,6 +819,8 @@ AUVAttitudeControl::control_depth(float dt)
   float vz_bar      = vz_tilde + _z_depth;
   float z_depth_dot = kz_depth*(-_z_depth + satFunction(vz_bar, delta_z_depth));
 
+  _z_depth += z_depth_dot*dt;
+
   float Fcz_Inertial = 19.0f* (vzd_dot + satFunction(-k2_depth*vz_bar,0.5) - z_depth_dot);  //mz = 19.0 kg
 
 
@@ -1049,6 +844,19 @@ float AUVAttitudeControl::satFunction(float x, float delta_x)
     return x;
   }
 
+}
+
+Vector<3> AUVAttitudeControl::sat3Function(Vector<3> x, float delta_x)
+{
+  for (int i=0; i < 3; ++i){
+    if (x(i) > delta_x) {
+       x(i) = delta_x;
+    }
+    else if (x(i) < -delta_x){
+      x(i) = -delta_x;
+    }
+  } 
+  return Vector<3> (x(0), x(1), x(2));
 }
 
 
@@ -1138,17 +946,18 @@ AUVAttitudeControl::control_att(float dt)
 
 
 	//Gain
-	float k1 = 0.8f; //0.5f; //0.1f;
-	float k2 = 7.0f; //2.0f; //1.0f;
-	Matrix<3, 3> K;
-	K.zero();
-	K(0, 0) = k2;  K(1, 1) = k2;   K(2, 2) = 1.5f;
+	float k1 = 0.3f; //0.8f; //0.5f; //0.1f;
+	//float k2 = 7.0f; //2.0f; //1.0f;
+	//Matrix<3, 3> K;
+	//K.zero();
+	//K(0, 0) = k2;  K(1, 1) = k2;   K(2, 2) = 1.5f;
 
 
 	Vector<3> Omega_d;
 	//In order: Vector and then scalar value, for multiplication 
 	Omega_d = (gamma_d % gamma) * k1 + gamma_d *  omega_d;
-
+  
+  Vector<3> Omega_d_dot(0.0f, 0.0f, 0.0f); //Approximation
 
 	//PX4_INFO("Debug Omega_d: %1.6f  %1.6f  %1.6f", (double)Omega_d(0), (double)Omega_d(1), (double)Omega_d(2));
 
@@ -1159,11 +968,19 @@ AUVAttitudeControl::control_att(float dt)
 	//J(1, 0) = 0.004f;   J(1, 1) = 0.2643f;  J(1, 2) = 0.007f;
 	//J(2, 0) = 0.005f;   J(2, 1) = 0.007f;   J(2, 2) = 0.3116f;
 
-  //For New-BlueROV1
-  J(0, 0) = 0.12242f;  J(0, 1) = 0.004f;   J(0, 2) = 0.005f;
-  J(1, 0) = 0.004f;    J(1, 1) = 0.3516f;  J(1, 2) = 0.007f;
-  J(2, 0) = 0.005f;    J(2, 1) = 0.007f;   J(2, 2) = 0.3448f;
+  //For New-BlueROV1, total innertial
+  J(0, 0) = 0.2024f;  J(0, 1) = 0.0000f;   J(0, 2) = 0.0000f;
+  J(1, 0) = 0.0000f;  J(1, 1) = 0.6516f;   J(1, 2) = 0.0000f;
+  J(2, 0) = 0.0000f;  J(2, 1) = 0.0000f;   J(2, 2) = 0.5448f;
 
+
+  Matrix<3, 3> J_inverted;
+  J_inverted.zero();
+  
+  //For New-BlueROV1, total innertial
+  J_inverted(0, 0) = 4.9407f;  J_inverted(0, 1) = 0.0000f;   J_inverted(0, 2) = 0.0000f;
+  J_inverted(1, 0) = 0.0000f;  J_inverted(1, 1) = 1.5347f;   J_inverted(1, 2) = 0.0000f;
+  J_inverted(2, 0) = 0.0000f;  J_inverted(2, 1) = 0.0000f;   J_inverted(2, 2) = 1.8355f;
 
 
 
@@ -1175,18 +992,86 @@ AUVAttitudeControl::control_att(float dt)
 	Vector<3> Omega_tilde = Omega - Omega_d; 
 
 
-	Vector<3> JOmega = J*Omega;
-	Vector3f temp (JOmega(0), JOmega(1), JOmega(2)); 
+	//Vector<3> JOmega = J*Omega;
+	//Vector3f temp (JOmega(0), JOmega(1), JOmega(2)); 
 
-	Vector3f temp1(Omega_d(0), Omega_d(1), Omega_d(2));
+	//Vector3f temp1(Omega_d(0), Omega_d(1), Omega_d(2));
 	
-	Vector3f temp2; 
-	temp2 = temp.cross(temp1);
+	//Vector3f temp2; 
+	//temp2 = temp.cross(temp1);
 
-	Vector<3> temp3(temp2(0), temp2(1), temp2(2));
+	//Vector<3> temp3(temp2(0), temp2(1), temp2(2));
 
-	Vector<3> Gamma_C;
-	Gamma_C = -(J * K)*Omega_tilde - temp3;
+	//Vector<3> Gamma_C;
+	//Gamma_C = -(J * K)*Omega_tilde - temp3;
+
+  //Integrator with simple integrator
+  Vector<3> z_omega_dot = Omega_tilde;
+
+  _z_omega     += z_omega_dot*dt; 
+
+  Vector<3> JOmega = J*Omega;
+
+  Vector3f JOmega_vector (JOmega(0), JOmega(1), JOmega(2)); 
+
+  Vector3f Omega_d_vector (Omega_d(0), Omega_d(1), Omega_d(2));
+  
+  Vector3f JOmega_Omega_d_vector; 
+  JOmega_Omega_d_vector = JOmega_vector.cross(Omega_d_vector);
+  Vector<3> JOmega_Omega_d(JOmega_Omega_d_vector(0), JOmega_Omega_d_vector(1), JOmega_Omega_d_vector(2));
+  
+  Vector<3>  G_feedforward;
+  G_feedforward = JOmega_Omega_d - J*Omega_d_dot; 
+
+  Matrix<3, 3> K_Omega;
+ 
+  K_Omega(0, 0) = 3.0f*0.2024f;  K_Omega(0, 1) = 0.0000f;        K_Omega(0, 2) = 0.0000f;
+  K_Omega(1, 0) = 0.0000f;       K_Omega(1, 1) = 3.0f*0.6516f;   K_Omega(1, 2) = 0.0000f;
+  K_Omega(2, 0) = 0.0000f;       K_Omega(2, 1) = 0.0000f;        K_Omega(2, 2) = 3.0f*0.5448f;
+
+  float Ki_Omega = 6.0;
+
+  float eta3 = 8.0;
+  
+
+  Vector3f e3_vector(e3(0), e3(1), e3(2));
+  Vector3f gamma_vector(gamma(0), gamma(1), gamma(2)); 
+  Vector3f e3RTe3_vector;
+  e3RTe3_vector = e3_vector.cross(gamma_vector);
+
+  Vector<3> e3RTe3 (e3RTe3_vector(0), e3RTe3_vector(1), e3RTe3_vector(2));
+
+  Vector<3> Gg;
+  Gg = e3RTe3*15.0*9.81*0.1;  //m*g*l e3 x RT e3
+
+  float a0 = 0.5;
+  float k0 = 20.0;
+  
+  Vector3f Omega_hat_vector (_Omega_hat(0), _Omega_hat(1), _Omega_hat(2));
+  
+  Vector3f JOmega_Omega_hat_vector; 
+  JOmega_Omega_hat_vector = JOmega_vector.cross(Omega_hat_vector);
+
+  Vector<3> JOmega_Omega_hat (JOmega_Omega_hat_vector(0), JOmega_Omega_hat_vector(1), JOmega_Omega_hat_vector(2));
+  
+  Vector<3> temp(0.0f, 0.0f, 0.0f);
+  Vector<3> G_control(_Gamma_c_x, _Gamma_c_y, _Gamma_c_z);
+
+  temp = JOmega_Omega_hat + G_control + Gg + _Delta_G_hat;
+
+  Vector<3> Omega_hat_dot(0.0f, 0.0f, 0.0f);
+  Omega_hat_dot = J_inverted*temp + (Omega - _Omega_hat)* k0;
+  
+  _Omega_hat += Omega_hat_dot*dt; //Approximation  
+
+  Vector<3> Delta_G_hat_dot;
+  Delta_G_hat_dot = J*(Omega - _Omega_hat)* a0*a0*k0*k0;
+  
+  _Delta_G_hat += Delta_G_hat_dot*dt; //Approximation
+
+  Vector<3> Gamma_C;
+  Gamma_C = -sat3Function(K_Omega*Omega_tilde, eta3) - _z_omega*Ki_Omega - G_feedforward - _Delta_G_hat;
+
 	
 	_Gamma_c_x = Gamma_C(0);
 	_Gamma_c_y = Gamma_C(1);
